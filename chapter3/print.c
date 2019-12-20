7b7b12c9b0872d6c3d6f4b3db5183f76923606d0
kajisun <papapahatena@gmail.com>
2019-12-20 02:27:13 +0000

    chapter3 completed: usage of printf

---
+++ chapter3/print.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello world!\n");
+    printf("Windows\tMicrosoft\n");
+    printf("MacOS\tApple\n");
+    printf("\n\n");
+    printf("Intel\t: Pentium4\n");
+    printf("AMD\t: Athlon64\n");
+    return 0;
+}
